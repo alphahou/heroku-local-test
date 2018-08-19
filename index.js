dc13ae2e31a491584057fc0643ac4de82ac4c1ab
Chenyi Hou <hcy13916978481@gmail.com>
2018-08-19 09:39:00 -0400

    add introduction in index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+	
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use('/public',express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'src/index.html')))
   .get('/draw-algorithm',(req, res) => res.sendFile(path.join(__dirname, 'src/drawAlgorithm.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
